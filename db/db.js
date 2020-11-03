acd09a335d359dc9d6e3f49eec8f5b40ba30c84e
juanperezu <juanperezu@gmail.com>
2020-11-02 20:12:06 -0500

    crud módulos cone clew cloud

---
+++ db/db.js
@@ -0,0 +1,29 @@
+const mysql =require('mysql');
+const mysqlConnection= mysql.createConnection({
+    // db creada en clew clouw
+host:'bprbeudfwg7eeipqbp9t-mysql.services.clever-cloud.com',
+user:'urappmxol0gahuzf',
+password:'piXeRzrY5CoTJ351xaU4',
+database:'bprbeudfwg7eeipqbp9t',
+multipleStatements:true
+
+/*
+host:'181.133.136.83',
+user:'mediatecnica',
+password:'123',
+database:'lab_mediatecnica',
+*/
+
+});
+
+mysqlConnection.connect(function(err){
+if(err){
+    console.error(err);
+    return;
+}else{
+
+    console.log('base de datos conectada');
+}
+
+})
+module.exports = mysqlConnection;
